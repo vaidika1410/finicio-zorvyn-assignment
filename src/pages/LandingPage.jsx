5137ec6fccf485ffeffc504da6ca3172f457482d
vaidika1410 <vaidikakaul20@gmail.com>
2026-04-03 13:25:09 +0530

    design landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,26 +5,31 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center px-4">
-      
+    <div className="flex flex-col items-center justify-center text-center px-4 overflow-hidden">
+
       <Navbar />
 
-      {/* Brand */}
-      <h1 className="text-5xl font-bold mb-4">
-        Finicio
-      </h1>
+      <div className="flex flex-col items-center justify-center h-screen text-center px-4 overflow-hidden">
+        {/* Brand */}
+        <h1 className="text-[18vh] font-bold mb-4 font-mono absolute top-15 text-[#cd774246]">
+          Finicio
+        </h1>
 
-      <p className="text-lg text-slate-400 mb-8 max-w-md">
-        Your financial clarity, simplified. Track, analyze, and grow your money with ease.
-      </p>
+        <div className="bg-[#ffaa7637] blur-2xl top-0 h-screen w-50 absolute flex items-center justify-center skew-17" />
+        <img className="z-2 absolute rotate-45 object-cover" src="connor-cobalt-1.png" alt="card image" />
 
-      {/* CTA Button */}
-      <button
-        onClick={() => navigate("/dashboard")}
-        className="bg-[#636EE1] hover:bg-[#4f58c9] transition px-6 py-3 rounded-xl font-medium"
-      >
-        View Dashboard →
-      </button>
+        <p className="text-xl text-[#ffffffe2] mb-8 w-150 mt-70 absolute z-4">
+          Track your money with clarity, understand your spending patterns, and take control of your financial future <br /> <span className="capitalize bg-clip-text text-transparent bg-linear-to-r from-[#cd774282] to-[#ffffffde] font-semibold">all in one place.</span>
+        </p>
+
+        {/* CTA Button */}
+        <button
+          onClick={() => navigate("/dashboard")}
+          className="border border-[#cd774282] backdrop-blur-sm transition px-15 py-4 rounded-xl font-medium absolute bottom-25 z-4 hover:bg-[#8b4e2876]"
+        >
+          View Dashboard →
+        </button>
+      </div>
 
     </div>
   );
