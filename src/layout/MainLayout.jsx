b98645d3a9e5083eaf9d9c9f0ec9b1c07c3367e3
vaidika1410 <vaidikakaul20@gmail.com>
2026-04-04 14:38:00 +0530

    feat: add a main layout page to handle structuring

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen">
+      
+      <Sidebar />
+
+      <div className="flex-1 ml-60">
+        <Navbar />
+
+        <div className="p-6 mt-16">
+          {children}
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default MainLayout;
