13c360651313c0cd67a2e18e00572f242adc7a36
vaidika1410 <vaidikakaul20@gmail.com>
2026-04-05 17:39:11 +0530

    style: update UI

--- src/components/RoleDropdown.jsx
+++ src/components/RoleDropdown.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { ChevronDown } from "lucide-react";
+import { useFinanceStore } from "../store/useFinanceStore";
 
 function RoleDropdown({ role, setRole }) {
   const [open, setOpen] = useState(false);
 
   const options = ["viewer", "admin"];
+  const {darkMode} = useFinanceStore()
 
   return (
     <div className="relative">
@@ -12,18 +13,18 @@ function RoleDropdown({ role, setRole }) {
       {/* Trigger */}
       <button
         onClick={() => setOpen(!open)}
-        className="flex items-center gap-1 lg:px-6 px-3 py-1
-        text-xs sm:text-sm text-[#ffffffcc]
-        transition cursor-pointer"
+        className={`flex items-center gap-1 lg:px-6 px-3 py-1
+        text-xs sm:text-sm ${darkMode ? "text-[#ffffffcc]" : "text-[#1b1b1be6]"}
+        transition cursor-pointer`}
       >
         {role.charAt(0).toUpperCase() + role.slice(1)}
       </button>
 
       {/* Dropdown */}
       {open && (
-        <div className="animate-dropdown absolute right-0 mt-2 w-28 rounded-lg 
-          bg-[#d7793e40] backdrop-blur-md border border-[#d7793e40]
-          shadow-[0_0_20px_rgba(215,121,62,0.15)] z-50 overflow-hidden"
+        <div className={`animate-dropdown absolute right-0 mt-2 w-28 rounded-lg 
+          ${darkMode ? "bg-[#774322da]" : "bg-[#7e4d2ff0]"} backdrop-blur-md border border-[#d7793e40]
+          shadow-[0_0_20px_rgba(215,121,62,0.15)] z-50 overflow-hidden`}
         >
           {options.map((item) => (
             <div
