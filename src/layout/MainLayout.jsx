79f7edb1b5abdc57cf75e006965bb74b650e9178
vaidika1410 <vaidikakaul20@gmail.com>
2026-04-05 16:24:12 +0530

    feat: update responsiveness of main layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,13 +4,15 @@ import Sidebar from "../components/Sidebar";
 const MainLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen">
-      
-      <Sidebar />
 
-      <div className="flex-1 ml-60">
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 md:ml-60">
         <Navbar />
 
-        <div className="p-6 mt-8">
+        <div className="p-4 sm:p-6 mt-16">
           {children}
         </div>
       </div>
