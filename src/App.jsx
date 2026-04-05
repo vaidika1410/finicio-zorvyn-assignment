2a9aebc45bd9f4212f9cf8e052afca69afa80b08
vaidika1410 <vaidikakaul20@gmail.com>
2026-04-05 17:43:28 +0530

    feat: add all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Insights from "./pages/Insights";
+import Analytics from "./pages/Analytics";
+import Settings from "./pages/Settings";
 import { useFinanceStore } from "./store/useFinanceStore";
 
 function App() {
@@ -10,14 +14,18 @@ function App() {
   return (
     <div
       className={`min-h-screen ${darkMode
-          ? "bg-[#000000] text-[#e2e8f0]"
-          : "bg-white text-black"
+        ? "bg-[#000000] text-[#e2e8f0]"
+        : "bg-white text-black"
         }`}
     >
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/insights" element={<Insights />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </div>
